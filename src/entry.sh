ed88066055f86e12dcdf627a0488b0354a31e406
silent001 <martinnr@live.co.za>
2021-09-14 11:18:44 +0200

    add trap to handle gracefully shutdown server

--- src/entry.sh
+++ src/entry.sh
@@ -3,14 +3,23 @@ export templdpath=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
 export SteamAppId=892970
 
+# NOTE: Without the wait the script exists before giving the server time to properly stop
+function clean_up {
+	kill -SIGINT $serverPID
+	wait
+}
 
-echo "Starting server PRESS CTRL-C to exit"
+# Setup trap
+trap "clean_up" SIGINT SIGTERM
 
-# Tip: Make a local copy of this script to avoid it being overwritten by steam.
-# NOTE: Minimum password length is 5 characters & Password cant be in the server name.
-# NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
 cd ${STEAMAPPDIR}
 # Add in Password length check
-./valheim_server.x86_64 -name "${SERVER_NAME}" -port ${PORT} -world "${WORLD}" -password "${SERVER_PASSWORD}"
+# NOTE: Minimum password length is 5 characters & Password cant be in the server name.
+
+./valheim_server.x86_64 -name "${SERVER_NAME}" -port ${PORT} -world "${WORLD}" -password "${SERVER_PASSWORD}" & serverPID=$!
+
+wait $serverPID
 
 export LD_LIBRARY_PATH=$templdpath
+
+exit 0
